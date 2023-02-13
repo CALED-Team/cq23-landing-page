27bb42f342ec3d51bda1e2007207332c866c0562
Jack Whelan <61770531+jw176@users.noreply.github.com>
2023-02-13 12:52:57 +1100

    Implemented accordion for FAQs

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+Accordion logic
+*/
+let accordions = document.querySelectorAll(".accordion");
+let accordion_states = Array(accordions.length).fill(null);
+
+function update_accordion(accordion_index, tab_index) {
+    let acc = accordions.item(accordion_index);
+
+    // remove open tab classes
+    acc.querySelectorAll(".tab-open").forEach((element) => {
+        element.classList.remove("tab-open");
+    });
+
+    // optionally add open tab class
+    if (accordion_states[accordion_index] != tab_index) {
+        acc.querySelectorAll(".tab").item(tab_index).classList.add("tab-open");
+        accordion_states[accordion_index] = tab_index;
+    } else {
+        accordion_states[accordion_index] = null;
+    }
+}
+
+accordions.forEach((accordion, acc_index) => {
+    accordion.querySelectorAll(".tab").forEach((tab, tab_index) => {
+        tab.addEventListener("click", () => {
+            update_accordion(acc_index, tab_index);
+        });
+    });
+});
