cb4298548747b32e09e3ea039d4d1428d8e4f788
Jack Whelan <61770531+jw176@users.noreply.github.com>
2023-02-14 18:32:13 +1100

    Added schedule section with tabs

--- index.js
+++ index.js
@@ -28,3 +28,25 @@ accordions.forEach((accordion, acc_index) => {
         });
     });
 });
+
+/*
+Schedule tabs logic
+*/
+document.querySelectorAll(".schedule").forEach((schedule_element) => {
+    schedule_element.querySelectorAll(".schedule-tab").forEach((tab, tab_index) => {
+        tab.addEventListener("click", () => {
+            schedule_element.querySelectorAll(".schedule-tab-content").forEach((element, index) => {
+                element.classList.remove("schedule-content-active");
+                if (index == tab_index) {
+                    element.classList.add("schedule-content-active");
+                }
+            })
+            schedule_element.querySelectorAll(".schedule-tab").forEach((element, index) => {
+                element.classList.remove("schedule-tab-active");
+                if (index == tab_index) {
+                    element.classList.add("schedule-tab-active");
+                }
+            })
+        })
+    })
+})
