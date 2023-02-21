9c6e0ceaa2bde2b199707c62c3b0e509ce1cf37c
Jack Whelan <61770531+jw176@users.noreply.github.com>
2023-02-21 19:14:35 +1100

    Added gallery carousel with buttons and an interval timer

--- index.js
+++ index.js
@@ -33,20 +33,105 @@ accordions.forEach((accordion, acc_index) => {
 Schedule tabs logic
 */
 document.querySelectorAll(".schedule").forEach((schedule_element) => {
-    schedule_element.querySelectorAll(".schedule-tab").forEach((tab, tab_index) => {
-        tab.addEventListener("click", () => {
-            schedule_element.querySelectorAll(".schedule-tab-content").forEach((element, index) => {
-                element.classList.remove("schedule-content-active");
-                if (index == tab_index) {
-                    element.classList.add("schedule-content-active");
+    schedule_element
+        .querySelectorAll(".schedule-tab")
+        .forEach((tab, tab_index) => {
+            tab.addEventListener("click", () => {
+                schedule_element
+                    .querySelectorAll(".schedule-tab-content")
+                    .forEach((element, index) => {
+                        element.classList.remove("schedule-content-active");
+                        if (index == tab_index) {
+                            element.classList.add("schedule-content-active");
+                        }
+                    });
+                schedule_element
+                    .querySelectorAll(".schedule-tab")
+                    .forEach((element, index) => {
+                        element.classList.remove("schedule-tab-active");
+                        if (index == tab_index) {
+                            element.classList.add("schedule-tab-active");
+                        }
+                    });
+            });
+        });
+});
+
+/*
+Image gallery carousel logic
+*/
+let gallery_moving_interval_ids = [];
+document
+    .querySelectorAll(".gallery")
+    .forEach((gallery_element, gallery_index) => {
+        const num_items =
+            gallery_element.querySelectorAll(".gallery-image img").length;
+
+        const select_index = (item_index) => {
+            gallery_element
+                .querySelectorAll(".gallery-image img")
+                .forEach((element, index) => {
+                    element.classList.remove("active-image");
+                    if (index == item_index) {
+                        element.classList.add("active-image");
+                    }
+                });
+            gallery_element
+                .querySelectorAll(".gallery-image-select li")
+                .forEach((element, index) => {
+                    element.classList.remove("active-image");
+                    if (index == item_index) {
+                        element.classList.add("active-image");
+                    }
+                });
+        };
+
+        // create click event listeners for the bullet points at the bottom
+        gallery_element
+            .querySelectorAll(".gallery-image-select li")
+            .forEach((tab, tab_index) => {
+                tab.addEventListener("click", () => {
+                    clearInterval(gallery_moving_interval_ids[gallery_index]);
+                    select_index(tab_index);
+                });
+            });
+
+        const find_active_index = () => {
+            let items = gallery_element.querySelectorAll(
+                ".gallery-image-select li"
+            );
+            for (let i = 0; i < items.length; i++) {
+                if (items.item(i).classList.contains("active-image")) {
+                    return i;
                 }
-            })
-            schedule_element.querySelectorAll(".schedule-tab").forEach((element, index) => {
-                element.classList.remove("schedule-tab-active");
-                if (index == tab_index) {
-                    element.classList.add("schedule-tab-active");
-                }
-            })
-        })
-    })
-})
+            }
+        };
+
+        const next_item = () => {
+            let active_index = find_active_index();
+            let new_active_index = (active_index + 1) % num_items;
+            select_index(new_active_index);
+        };
+
+        const previous_item = () => {
+            let active_index = find_active_index();
+            let new_active_index = (active_index - 1 + num_items) % num_items;
+            select_index(new_active_index);
+        };
+
+        // add click handlers to the arrows
+        gallery_element
+            .querySelector(".left-arrow")
+            .addEventListener("click", () => {
+                clearInterval(gallery_moving_interval_ids[gallery_index]);
+                previous_item();
+            });
+        gallery_element
+            .querySelector(".right-arrow")
+            .addEventListener("click", () => {
+                clearInterval(gallery_moving_interval_ids[gallery_index]);
+                next_item();
+            });
+
+        gallery_moving_interval_ids.push(setInterval(next_item, 3000));
+    });
