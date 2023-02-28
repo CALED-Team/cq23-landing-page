fc286bb0cee94399bdcaef87687b4bef8990cbe0
Jack Whelan <61770531+jw176@users.noreply.github.com>
2023-02-28 15:28:56 +1100

    Feature responsiveness (#10)
    * made the header responsive
    
    * made hero section responsive
    
    * minor improvements to hero section
    
    * improvements for wide vs narrow(text) sections
    
    * improved responsiveness for the diagrams in the about section
    
    * made prize section responsive
    
    * responsiveness for image carousel
    
    * changed video position for small screens
    
    * Revert "changed video position for small screens"
    
    This reverts commit 935a7b0a6d3db10f2fe5581a319d355cae52cca2.
    
    * smaller hero video for mobile
    
    * made footer responsive
    
    * made the schedule responsive
    
    * improved responsiveness for faqs accordion
    
    * improved responsiveness for schedule
    
    * shortened some headings
    
    * added favicon
    
    * Update build.sh
    
    * Update style.css
    
    * Revert "Update style.css"
    
    This reverts commit 47c99255e1db7f57db2ca800a6e2da96662df762.
    
    * Update style.css
    
    * hopefully fix responsiveness bug on ipad

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ cp font_imports.css build
 cp index.html build
 cp style.css build
 cp index.js build
+cp android-chrome-192x192.png build
+cp android-chrome-512x512.png build
+cp apple-touch-icon.png build
+cp favicon-16x16.png build
+cp favicon-32x32.png build
+cp favicon.ico build
+cp site.webmanifest build
